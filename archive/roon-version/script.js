f95c8bc5df310b1f12f3f482328242c868c7a6c9
John <john@JohndeMacBook-Pro.local>
2020-04-08 06:15:38 +0800

    initial commit

---
+++ archive/roon-version/script.js
@@ -0,0 +1,24 @@
+function show(target){
+  //hide everything first...
+  var items = document.querySelectorAll('.items');
+  for (i = 0; i < items.length; i++) {
+    items[i].classList.remove('shown');
+  }
+  //then show target divs
+  var selected = document.querySelectorAll(target);
+  for (i = 0; i < selected.length; i++) {
+    selected[i].classList.add('shown');
+  }
+
+  //remove highlights from nav first...
+  var filter = document.querySelectorAll('.filter');
+  for (i = 0; i < filter.length; i++) {
+    filter[i].classList.remove('selected');
+  }
+
+  //then  highlight the current
+  if (target !== ".items"){
+    //"item"(show all) is removed from the "selected"
+    event.target.classList.add('selected');
+  }
+}
--- sb/sidebarjava.js
+++ sb/sidebarjava.js
@@ -1,19 +1,28 @@
 var div1 = document.createElement('div');
+//create a variable defined as a div
 div1.className = 'sidebar';
+//give the div a classname
 document.getElementsByTagName('body')[0].appendChild(div1);
+//append the div into the parent body div
 
 var div2 = document.createElement('div');
 div2.className = 'content';
 document.getElementsByTagName('body')[0].appendChild(div2);
+//the same
 
 
 var pages = ["home", "writings", "drawings", "sketchbook",
 "prints", "travel", "organizations", "contact", "links"];
 
 var sidebar = document.querySelector(".sidebar");
+//store the contents of sidebar
 
 for (let i =0; i < pages.length; i++) {
+  //createt a loop of values in the array, each time a different value.
   console.log(pages[i]);
+  //run the loop with the array.
   var link = `<a href ="${pages[i]}.html" class="sidebar-link"> ${pages[i]}</a>`;
+  //define variable link: link name = href link that is made up of the array.
   sidebar.innerHTML += link;
+  //insert varibale "link" into the sidebar;
 }
